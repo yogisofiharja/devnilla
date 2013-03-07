4179b98d63669ffedd8ef3cf521ebddb5b018b02
yogisofiharja <hardja.ogie@gmail.com>
2013-03-07 23:53:59 +0700

    pake twig

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -5,8 +5,13 @@ class Welcome extends CI_Controller {
 	
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('container');
-		$this->load->view('footer');
+		$this->load->spark('Twiggy/0.8.5');
+		$this->load->helper('url'); // load the CodeIgniter URL helper
+		// map the base_url() function as a Twig function 
+		$this->twiggy->register_function('base_url'); 
+		//$this->load->view('header');
+		//$this->load->view('container');
+		//$this->load->view('footer');
+		$this->twiggy->display();
 	}
 }
--- application/views/footer.php
+++ application/views/footer.php
@@ -10,7 +10,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+    <!--<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>-->
     <script src="<?php echo base_url();?>bootstrap/docs/assets/js/jquery.js"></script>
     <script src="<?php echo base_url();?>bootstrap/docs/assets/js/google-code-prettify/prettify.js"></script>
     <script src="<?php echo base_url();?>bootstrap/docs/assets/js/bootstrap-transition.js"></script>
