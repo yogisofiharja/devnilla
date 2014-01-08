1cfc8bb7cebb699a5a0484b7eab7ea0f4b640690
root <root@yogisofiharja.(none)>
2014-01-09 00:54:23 +0700

    design baru

--- application/views/index.php
+++ application/views/index.php
@@ -100,8 +100,7 @@
     </div>   
 	
 	<div class="control-nav">
-        <a id="prevslide" class="load-item"><i class="font-icon-arrow-simple-left"></i></a>
-        <a id="nextslide" class="load-item"><i class="font-icon-arrow-simple-right"></i></a>
+        
         <ul id="slide-list"></ul>
         
         <a id="nextsection" href="#work"><i class="font-icon-arrow-simple-down"></i></a>
